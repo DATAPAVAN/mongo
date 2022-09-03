ab4148eba343e850a51d519384a077399a78de68
pavan <datapavankumar@gmail.com>
2022-09-03 15:21:45 +0530

    this is my first code push in git

--- mongotest1.py
+++ mongotest1.py
@@ -9,5 +9,10 @@ d={'name':'pavan','emailid':'kkbk@gmail.com'}
 db1=client['mongotest1']
 coll=db1['test']
 coll.insert_one(d)
+record = coll.find()
+for i in record:
+    print(i)
+coll.find.prettyprint()
+
 
 
